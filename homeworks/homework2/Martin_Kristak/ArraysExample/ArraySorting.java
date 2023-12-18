56829ad5f0e9818655a48de832a9120eb5f28d4f
martin <martinkristak62@gmail.com>
2023-12-18 12:01:16 +0100

    Homework_final

---
+++ homeworks/homework2/Martin_Kristak/ArraysExample/ArraySorting.java
@@ -0,0 +1,29 @@
+package homeworks.homework2.Martin_Kristak.ArraysExample;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+        // Sorting an array
+        int[] numbers = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 15, 10, 0};
+        Arrays.sort(numbers);
+
+        System.out.println("Sorted array:");
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println(); // New line for separation
+
+        // Sorting an ArrayList
+        ArrayList<String> fruits = new ArrayList<>(Arrays.asList("Apple", "Banana", "Orange"));
+        Collections.sort(fruits);
+
+        System.out.println("Sorted fruits:");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}
+
---
+++ homeworks/homework2/Martin_Kristak/ArraysExample/SearchingExample.java
@@ -0,0 +1,43 @@
+package homeworks.homework2.Martin_Kristak.ArraysExample;
+
+import java.util.Arrays;
+
+public class SearchingExample {
+    public static void main(String[] args) {
+        // Linear search in an array
+        int[] numbers = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
+        int target = 9;
+        int index = linearSearch(numbers, target);
+
+        System.out.println("Linear search result:");
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index " + index);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+
+        // Binary search in a sorted array
+        Arrays.sort(numbers);
+        target = 5;
+        index = Arrays.binarySearch(numbers, target);
+
+        System.out.println("Binary search result:");
+        if (index >= 0) {
+            System.out.println("Element " + target + " found at index " + index);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+    }
+
+    // Linear search function
+    private static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1; // Element not found
+    }
+}
+
+
